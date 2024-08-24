7b0a6177bd31f1b2b025df7a3b2ff024f1ede7a6
ma7moudsalem1 <ma7moudsalem1@gmail.com>
2024-08-24 19:57:59 +0300

    clean up

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,12 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if (! $request->expectsJson()) {
+            if($request->is('*emp*') && !$request->is('*employees*')){
+                return route('emp_login');
+            }
+            return route('login');
+        }
+        return null;
     }
 }
